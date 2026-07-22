4ad3066351ac2e9fb2e1cd0573883774bdc0a088
BlainPeng <101913530+wjm777@users.noreply.github.com>
2026-07-22 15:19:07 +0800

    Create status.js

---
+++ status.js
@@ -0,0 +1,13 @@
+window.ServiceStatus = {
+  code: 0,
+  message: "",
+  data: {
+    maintenance: {
+      enabled: true,
+      title: "System Maintenance",
+      message: "亲爱的用户，为了配合 Visa 系统的技术升级，MPay 将于2026年7月22日11点（UTC+0）短暂停止对外服务。预计升级时间将持续数小时，具体视升级进度而定。升级期间，用户无需操作任何事项。感谢您的理解与支持！",
+      start_at: "2026-02-04T02:00:00Z",
+      end_at: "2026-02-04T04:00:00Z"
+    }
+  }
+}
