56099728b81b1e444e078f37876075ba7d390af0
BlainPeng <101913530+wjm777@users.noreply.github.com>
2026-07-22 16:04:55 +0800

    Update status.js

--- status.js
+++ status.js
@@ -4,7 +4,7 @@ window.ServiceStatus = {
   data: {
     maintenance: {
       enabled: false,
-      title: "System Maintenance",
+      title: "System Maintenance12",
       message: "Visa services are upgrading. Please try again later.",
       start_at: "2026-02-04T02:00:00Z",
       end_at: "2026-02-04T04:00:00Z"
